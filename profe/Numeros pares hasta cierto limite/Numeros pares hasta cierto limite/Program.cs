16a4f3ce196e631092c8a33aa2bc633dc3e4483a
Moises Alcantara <148722914+Black-shadow-tech@users.noreply.github.com>
2023-12-08 18:57:25 -0400

    Bucles

---
+++ profe/Numeros pares hasta cierto limite/Numeros pares hasta cierto limite/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Numeros_pares_hasta_cierto_limite
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Limite de numeros a resivir: ");
+            int limite = Convert.ToInt32(Console.ReadLine());//por si al cliente se le ocurre definir el limite XD
+
+            Console.Write("Digite un numero par: ");
+            int i = Convert.ToInt32(Console.ReadLine());
+
+            if (i %2 ==0) // si no es par no entra al bucle
+            {
+                Console.WriteLine("\n");
+                while (i <= limite)// el iterador llegue hacer igual q el limite salda del bucle..
+                {
+                    Console.WriteLine(i);
+                    i += 2;
+                }
+                Console.WriteLine("\n");//deberia hacer un salto de linia interno XD
+            }
+            else
+            {
+                Console.WriteLine("El numero {0} no es un numero par XD",i);
+                Console.WriteLine("\n");
+            }
+        }
+    }
+}
---
+++ profe/Suma de numeros del 1 al 100/Suma de numeros del 1 al 100/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Suma_de_numeros_del_1_al_100
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int suma = 0;
+
+            Console.Write("Digite el parametro de numeros a sumar: ");//esto es por si al cliente se le ocurre q en vez de 100 sea un 200 0 un 50...
+            int para = Convert.ToInt32(Console.ReadLine());
+
+            for (int i=1; i<=para; i++)
+            {
+                suma += i;
+            }
+            Console.WriteLine("\n");
+            Console.WriteLine("Este es la suma de su conjunto de elementos: {0}",suma);
+            Console.WriteLine("\n");
+        }
+    }
+}
